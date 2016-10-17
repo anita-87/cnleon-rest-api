3f8eee684b9546b4af6a1f8095a9783987068d4f
anita-87 <ana.rodriguez.fernandez.87@gmail.com>
2016-10-17 22:34:50 +0200

    Adding comments to the files uploaded in the initial commit,

--- src/main/java/com/cnleon/repositories/SwimmerRepository.java
+++ src/main/java/com/cnleon/repositories/SwimmerRepository.java
@@ -5,10 +5,18 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 /**
+ * Repository to access to the Swimmer entities stored in the DB.
+ * @see org.springframework.data.mongodb.repository.MongoRepository
+ *
  * Created by anita on 12/10/16.
  */
 @Repository
 public interface SwimmerRepository extends MongoRepository<Swimmer, String> {
 
+    /**
+     * Method to find a swimmer by its identifier.
+     * @param id - the identifier of the swimmer.
+     * @return a swimmer instance if its found or null if is not.
+     */
     Swimmer findById(String id);
 }

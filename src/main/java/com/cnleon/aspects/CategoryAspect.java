b2c551a2160a90e5b87fcf8fb267ee67b5e555fc
Ana Rodríguez Fernandez <ana.rodriguez.fernandez.87@gmail.com>
2016-10-17 23:47:59 +0200

    - Added new request in the API for /swimmers that will return all the swimmers available in the DB. - Added missing documentation for the Category Aspect Class.

--- src/main/java/com/cnleon/aspects/CategoryAspect.java
+++ src/main/java/com/cnleon/aspects/CategoryAspect.java
@@ -8,13 +8,20 @@ import org.aspectj.lang.annotation.Aspect;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Component;
 
 import java.time.LocalDate;
 import java.time.ZoneId;
 import java.util.Date;
+import java.util.Iterator;
+import java.util.List;
 
 /**
+ * Aspect to handle the categories for the swimmers.
+ * Categories are not stored in the DB, the are calculated every time one of the methods
+ * indicated below (in the pointcuts) are called in any other part of the code.
+ *
  * Created by anita on 15/10/16.
  */
 
@@ -22,8 +29,12 @@ import java.util.Date;
 @Component
 public class CategoryAspect {
 
+    /**
+     * Logger instance for the service.
+     */
     private final Logger logger = LoggerFactory.getLogger(this.getClass());
 
+    //List of all the properties for the years of all the categories
     @Value("${swimmers.abs.masc.low}")
     private Integer absMascLow;
 
@@ -96,6 +107,12 @@ public class CategoryAspect {
     @Value("${swimmers.prebbenj.fem.high}")
     private Integer prebenjFemHigh;
 
+    /**
+     * Method to calculate the category for a single swimmer object.
+     * The aspect is called automatically every time the method "findById" is called.
+     * {@link com.cnleon.repositories.SwimmerRepository#findById(String)}
+     * @param swimmer - the swimmer to calculate the category.
+     */
     @AfterReturning(pointcut = "execution(* com.cnleon.repositories.SwimmerRepository.findById(..))",
     returning = "swimmer")
     public void setSwimmerCategory(Object swimmer){
@@ -110,6 +127,30 @@ public class CategoryAspect {
         }
     }
 
+    /**
+     * Method to calculate the category for a list of swimmers.
+     * Iterates through the list of swimmers and the call the method "setSwimmerCategory"
+     * @see com.cnleon.aspects.CategoryAspect#setSwimmerCategory(Object)
+     * The aspect is called automatically every time the method "findAll" from MongoRepository finishes.
+     * {@link MongoRepository#findAll()}
+     * @param swimmers - the list of swimmers obtained from the DB.
+     */
+    @AfterReturning(pointcut = "execution(* org.springframework.data.mongodb.repository.MongoRepository.findAll(..))",
+    returning = "swimmers")
+    public void setSwimmersCategoryFromList(Object swimmers){
+        Iterator<Swimmer> swimmerIterator = ((List<Swimmer>) swimmers).iterator();
+        while (swimmerIterator.hasNext()){
+            Swimmer swimmer = swimmerIterator.next();
+            logger.debug("Calculating category for swimmer " + swimmer.getId());
+            this.setSwimmerCategory(swimmer);
+        }
+    }
+
+    /**
+     * Private method to calculate the category for male swimmers.
+     * @param birthDate - the birth date of the male swimmer.
+     * @return the category enum of the swimmer or null if the swimmer does not match any category
+     */
     private Category calculateMaleCategory(Date birthDate){
         LocalDate date = birthDate.toInstant().atZone(ZoneId.systemDefault()).toLocalDate();
         logger.debug("Calculating male category for year " + date.getYear());
@@ -131,6 +172,11 @@ public class CategoryAspect {
         return null;
     }
 
+    /**
+     * Private method to calculate the category for female swimmers.
+     * @param birthDate - the birth date of the female swimmer.
+     * @return the category enum of the swimmer or null if the swimmer does not match any category
+     */
     private Category calculateFemaleCategory(Date birthDate){
         LocalDate date = birthDate.toInstant().atZone(ZoneId.systemDefault()).toLocalDate();
         logger.debug("Calculating female category for year " + date.getYear());
